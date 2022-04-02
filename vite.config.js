674e8ecba06c5a463746a2922e762bf2b0146fe7
Chris Drake <christopher.a.drake@gmail.com>
2022-04-02 18:41:31 -0400

    Update path for index and removed asset limit size

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import { defineConfig } from "vite";
 // vite.config.js
 export default defineConfig({
   base: '/niivue-vite/',
-  assetsInclude: ['**/*.wasm'],
-  assetsInlineLimit: '2048' // 2kb
+  assetsInclude: ['**/*.gz'],
 })
